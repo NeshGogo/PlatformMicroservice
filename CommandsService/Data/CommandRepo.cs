9321c1e8850b49cd9320c0a66114335cb4be9416
NeshGogo <rafaelaguero10@hotmail.com>
2023-06-20 20:46:05 -0400

    data layer to commandservice

---
+++ CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,54 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data;
+public class CommandRepo : ICommandRepo
+{
+  private readonly AppDbContext _context;
+
+  public CommandRepo(AppDbContext context)
+  {
+    _context = context;
+  }
+  public void CreateCommand(int platformId, Command command)
+  {
+    if (command == null)
+      throw new ArgumentNullException(nameof(command));
+    command.PlatformId = platformId;
+    _context.Commands.Add(command);
+  }
+
+  public void CreatePlatform(Platform platform)
+  {
+    if (platform == null)
+      throw new ArgumentNullException(nameof(platform));
+
+    _context.Platforms.Add(platform);
+  }
+
+  public Command GetCommand(int platformId, int commandId)
+  {
+    return _context.Commands.FirstOrDefault(p => p.Id == commandId && p.PlatformId == platformId);
+  }
+
+  public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+  {
+    return _context.Commands
+      .Where(p => p.PlatformId == platformId)
+      .OrderBy(p => p.Platform.Name);
+  }
+
+  public IEnumerable<Platform> GetPlatforms()
+  {
+    return _context.Platforms;
+  }
+
+  public bool PlatformExists(int platformId)
+  {
+    return _context.Platforms.Any(p => p.Id == platformId);
+  }
+
+  public bool SaveChanges()
+  {
+    return _context.SaveChanges() >= 0;
+  }
+}
--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,6 +1,11 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("CommandDB"));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
